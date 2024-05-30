3a7b729149181e53a3d8b8bed9bf8c7f33bb942e
cnwabuoku <nwabuokujohn@gmail.com>
2024-05-30 13:25:13 -0400

    Category class

---
+++ Model/Category.cs
@@ -0,0 +1,13 @@
+﻿namespace PersonalFinance.Model;
+
+public class Category
+{
+	public string Name { get; set; }
+	public double Limit { get; set; }
+
+	public Category(string name, double limit)
+	{
+		Name = name;
+		Limit = limit;
+	}
+}
