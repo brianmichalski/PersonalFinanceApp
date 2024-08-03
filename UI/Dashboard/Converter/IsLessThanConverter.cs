fd3d8398ea1f9a30156efce4743b7c3f4469afee
brianmichalski <90117970+brianmichalski@users.noreply.github.com>
2024-08-02 22:39:58 -0400

    UI adjustment for coloring expenses in transactions table

---
+++ UI/Dashboard/Converter/IsLessThanConverter.cs
@@ -0,0 +1,22 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+
+namespace PersonalFinanceApp.UI.Dashboard.Converter
+{
+    // source: https://stackoverflow.com/questions/71779176/xaml-datagrid-set-rowstyle-if-value-greater-than
+    public class IsLessThanConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            double intValue = (double)value;
+            double compareToValue = Double.Parse(parameter.ToString() ?? string.Empty);
+
+            return (intValue < compareToValue);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
